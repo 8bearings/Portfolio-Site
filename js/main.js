f004ada3a357f2e1fd21fbcf24c221f6e3b9a864
Elliott Watson <elliottwatson0@gmail.com>
2021-09-28 03:06:15 -0400

    added babel and parcel along with new seperated JS. -sizing on photo page, added morphing text to index page

--- js/main.js
+++ js/main.js
@@ -1,36 +1,96 @@
-// My JS is bad, sorry
-window.onload = function() {
-let grid = document.getElementById("the--grid");
-let loadingAnimation = document.getElementById("loading");
+/*
+	This pen cleverly utilizes SVG filters to create a "Morphing Text" effect. Essentially, it layers 2 text elements on top of each other, and blurs them depending on which text element should be more visible. Once the blurring is applied, both texts are fed through a threshold filter together, which produces the "gooey" effect. Check the CSS - Comment the #container rule's filter out to see how the blurring works!
+*/
+window.addEventListener('load', function() {
+const elts = {
+	text1: document.getElementById("text1"),
+	text2: document.getElementById("text2")
+};
 
+// The strings to morph between. You can change these to anything you want!
+const texts = [
+	"Elliott Watson",
+	"Software Engineer",
+	"Web Developer",
+	"Photographer",
+	"Full Stack Engineer",
+	"Programmer"
+	
+];
 
+// Controls the speed of morphing.
+const morphTime = 2.2;
+const cooldownTime = 0.20;
 
-  grid.classList.remove('loading');
-  loadingAnimation.classList.add('hide');
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+	morph -= cooldown;
+	cooldown = 0;
+	
+	let fraction = morph / morphTime;
+	
+	if (fraction > 1) {
+		cooldown = cooldownTime;
+		fraction = 1;
+	}
+	
+	setMorph(fraction);
 }
 
-let firstHidden = document.getElementById('imageHiding1');
-let secondHidden = document.getElementById('imageHiding2');
-let thirdHidden = document.getElementById('imageHiding3');
-let forthHidden = document.getElementById('imageHiding4');
-let fifthHidden = document.getElementById('imageHiding5');
-let sixthHidden = document.getElementById('imageHiding6');
-let seventhHidden = document.getElementById('imageHiding7');
-
-function removeHidden() {
-  if (firstHidden.classList.contains('hidden')) {
-    firstHidden.classList.remove('hidden');
-  } else if (secondHidden.classList.contains('hidden')) {
-    secondHidden.classList.remove('hidden');
-  } else if (thirdHidden.classList.contains('hidden')) {
-    thirdHidden.classList.remove('hidden');
-  } else if (forthHidden.classList.contains('hidden')) {
-    forthHidden.classList.remove('hidden');
-  } else if (fifthHidden.classList.contains('hidden')) {
-    fifthHidden.classList.remove('hidden');
-  } else if (sixthHidden.classList.contains('hidden')) {
-    sixthHidden.classList.remove('hidden');
-  } else if (seventhHidden.classList.contains('hidden')) {
-    seventhHidden.classList.remove('hidden');
-  }
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+	// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+	
+	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	fraction = 1 - fraction;
+	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	elts.text1.textContent = texts[textIndex % texts.length];
+	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 }
+
+function doCooldown() {
+	morph = 0;
+	
+	elts.text2.style.filter = "";
+	elts.text2.style.opacity = "100%";
+	
+	elts.text1.style.filter = "";
+	elts.text1.style.opacity = "0%";
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+	requestAnimationFrame(animate);
+	
+	let newTime = new Date();
+	let shouldIncrementIndex = cooldown > 0;
+	let dt = (newTime - time) / 1000;
+	time = newTime;
+	
+	cooldown -= dt;
+	
+	if (cooldown <= 0) {
+		if (shouldIncrementIndex) {
+			textIndex++;
+		}
+		
+		doMorph();
+	} else {
+		doCooldown();
+	}
+}
+
+// Start the animation.
+animate();})
+
---
+++ js/photo.js
@@ -0,0 +1,39 @@
+// Header
+window.onload = function() {
+let grid = document.getElementById("the--grid");
+let loadingAnimation = document.getElementById("loading");
+
+
+
+  grid.classList.remove('loading');
+  loadingAnimation.classList.add('hide');
+}
+
+let firstHidden = document.getElementById('imageHiding1');
+let secondHidden = document.getElementById('imageHiding2');
+let thirdHidden = document.getElementById('imageHiding3');
+let forthHidden = document.getElementById('imageHiding4');
+let fifthHidden = document.getElementById('imageHiding5');
+let sixthHidden = document.getElementById('imageHiding6');
+let seventhHidden = document.getElementById('imageHiding7');
+
+function removeHidden() {
+  if (firstHidden.classList.contains('hidden')) {
+    firstHidden.classList.remove('hidden');
+  } else if (secondHidden.classList.contains('hidden')) {
+    secondHidden.classList.remove('hidden');
+  } else if (thirdHidden.classList.contains('hidden')) {
+    thirdHidden.classList.remove('hidden');
+  } else if (forthHidden.classList.contains('hidden')) {
+    forthHidden.classList.remove('hidden');
+  } else if (fifthHidden.classList.contains('hidden')) {
+    fifthHidden.classList.remove('hidden');
+  } else if (sixthHidden.classList.contains('hidden')) {
+    sixthHidden.classList.remove('hidden');
+  } else if (seventhHidden.classList.contains('hidden')) {
+    seventhHidden.classList.remove('hidden');
+  }
+}
+
+// Body
+
